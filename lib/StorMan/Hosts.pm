c4d3dd7bf83d1ec276a758ae9ef1de5c9767e039
Patrick Schmid <schmid@phys.ethz.ch>
2014-01-15 14:22:33 +0100

    make dashboard alive

---
+++ lib/StorMan/Hosts.pm
@@ -0,0 +1,98 @@
+package StorMan::Hosts;
+
+use 5.010;
+use strict;
+use warnings;
+use StorMan::Common;
+use Net::Ping;
+
+use Exporter 'import';
+our @EXPORT = qw(
+    get_fsinfo
+);
+
+sub get_fsinfo {
+    my %fsinfo;
+    foreach my $server ( 'phd-bkp-gw' ) {
+        my @mounts;
+        @mounts = remotewrapper_command( $server, 'BaNG/bang_df' );
+
+        foreach my $mount (@mounts) {
+            $mount =~ qr{
+                        ^(?<filesystem> [\/\w\d-]+)
+                        \s+(?<fstyp> [\w\d]+)
+                        \s+(?<blocks> [\d]+)
+                        \s+(?<used> [\d]+)
+                        \s+(?<available>[\d]+)
+                        \s+(?<usedper> [\d]+)
+                        .\s+(?<mountpt> [\/\w\d-]+)
+            }x;
+
+            $fsinfo{$server}{$+{mountpt}} = {
+                'filesystem' => $+{filesystem},
+                'mount'      => $+{mountpt},
+                'fstyp'      => $+{fstyp},
+                'blocks'     => num2human($+{blocks}),
+                'used'       => num2human($+{used}*1024,1024),
+                'available'  => num2human($+{available}*1024,1024),
+                'freediff'   => "",
+                'used_per'   => $+{usedper},
+                'css_class'  => check_fill_level($+{usedper}),
+            };
+        }
+
+        if ($server eq "phd-bkp-gw") {
+            @mounts = remotewrapper_command( $server, 'BaNG/bang_di' ) ;
+            foreach my $mount (@mounts) {
+                $mount =~ qr{
+                ^(?<filesystem> [\/\w\d-]+)
+                \s+(?<fstyp>[\w\d]+)
+                \s+(?<blocks>[\d]+)
+                \s+(?<used>[\d]+)
+                \s+(?<available>[\d]+)
+                \s+(?<free>[\d]+)
+                \s+(?<usedper>[\d]+)
+                .\s+(?<mountpt>[\/\w\d-]+)
+                }x;
+
+                my $freediff    = $+{free} - $+{available};
+                my $freediffper = 100 / $+{free} * $freediff;
+
+                $fsinfo{$server}{$+{mountpt}}{freediff} = ( $freediffper > 10 ) ? num2human($freediff*1024,1024) : "" ;
+            }
+        }
+
+    }
+
+    return \%fsinfo;
+}
+
+sub check_fill_level {
+    my ($level) = @_;
+    my $css_class = '';
+
+    if ( $level > 98 ) {
+        $css_class = "alert_red";
+    } elsif ( $level > 90 ) {
+        $css_class = "alert_orange";
+    } elsif ( $level > 80 ) {
+        $css_class = "alert_yellow";
+    }
+
+    return $css_class;
+}
+
+#################################
+# RemoteWrapper
+#
+sub remotewrapper_command {
+    my ($remoteHost, $remoteCommand, $remoteArgument) = @_;
+    $remoteArgument ||= '';
+
+    my $results = `ssh -o IdentitiesOnly=yes -i /var/www/.ssh/remotesshwrapper root\@$remoteHost /usr/local/bin/remotesshwrapper $remoteCommand $remoteArgument 2>/dev/null`;
+    my @results = split( "\n", $results );
+
+    return @results;
+}
+
+1;
--- lib/StorMan/Routes.pm
+++ lib/StorMan/Routes.pm
@@ -1,7 +1,26 @@
 package StorMan::Routes;
+
+use 5.010;
+use strict;
+use warnings;
 use Dancer ':syntax';
+use StorMan::Common;
+use StorMan::Hosts;
+
+prefix undef;
 
 get '/' => sub {
+
     template 'dashboard.tt', {
-        };
-        };
+    };
+};
+
+get '/fsinfo_report' => sub {
+
+    template 'dashboard-fsinfo' => {
+        fsinfo => get_fsinfo(),
+        },{
+        layout => 0 };
+};
+
+
