bc31e01b2b0348040ec33d9911f4de21a740494b
Patrick Schmid <schmid@phys.ethz.ch>
2014-01-16 10:49:27 +0100

    get iscsi_sessions_info from phd-bkp-gw, phd-san-gw1 & phd-san-gw2

--- lib/StorMan/Iscsi.pm
+++ lib/StorMan/Iscsi.pm
@@ -15,33 +15,30 @@ my $iscsiadm = '/sbin/iscsiadm';
 sub get_iscsi_sessions {
     my @sessions;
     my %sessioninfo;
-    my $server = "phd-bkp-gw";
 
-#    @sessions =  `$iscsiadm -m session`;
-    @sessions = remotewrapper_command( $server, 'StorMan/iscsi_sessions' );
+    foreach my $server ( 'phd-bkp-gw','phd-san-gw1','phd-san-gw2' ) {
 
+        @sessions = remotewrapper_command( $server, 'StorMan/iscsi_sessions' );
 
-    # tcp: [1] 172.31.108.18:3260,1 iqn.2011-05.ch.ethz.phys:bkp18
+        foreach my $session (@sessions) {
+            $session =~ qr{
+            ^(?<protocol> [\w]+):
+            \s+\[(?<session_id> [\d]+)\]
+            \s+(?<host_ip> [^:]*)
+            :(?<port> [^,]+)
+            ,(?<whatever>[\d]+)
+            \s+(?<iqn> [^\$]+)
+            }x;
 
-    foreach my $session (@sessions) {
-        $session =~ qr{
-        ^(?<protocol> [\w]+):
-        \s+\[(?<session_id> [\d]+)\]
-        \s+(?<host_ip> [^:]*)
-        :(?<port> [^,]+)
-        ,(?<whatever>[\d]+)
-        \s+(?<iqn> [^\$]+)
-        }x;
-
-        $sessioninfo{$server}{$+{session_id}} = {
-            'protocol' => $+{protocol},
-            'host_ip'  => $+{host_ip},
-            'port'     => $+{port},
-            'whatever' => $+{whatever},
-            'iqn'      => $+{iqn},
-        };
+            $sessioninfo{$server}{$+{session_id}} = {
+                'protocol' => $+{protocol},
+                'host_ip'  => $+{host_ip},
+                'port'     => $+{port},
+                'whatever' => $+{whatever},
+                'iqn'      => $+{iqn},
+            };
+        }
     }
-
     return \%sessioninfo;
 }
 
