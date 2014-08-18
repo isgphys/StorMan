426bfd70fd47a556e7f540c28ae8d67d3fdb27b4
Patrick Schmid <schmid@phys.ethz.ch>
2014-08-18 14:52:41 +0200

    StorMan-Web: check for existing config.yml

--- StorMan-Web.pl
+++ StorMan-Web.pl
@@ -1,7 +1,13 @@
 #!/usr/bin/env perl
 use lib '/opt/StorMan/lib';
-
+use Cwd qw( abs_path );
 use Dancer;
 use StorMan::Routes;
 
-Dancer->dance;
+my $prefix = dirname( abs_path($0) );
+
+if ( -e "$prefix/config.yml" ) {
+    Dancer->dance;
+} else {
+    print "Error config.yml missing!\n";
+}
