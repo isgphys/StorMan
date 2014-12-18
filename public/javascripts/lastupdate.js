aa88311c8926e0add06e343b743c5de266f1eee6
Patrick Schmid <schmid@phys.ethz.ch>
2014-12-18 10:05:01 +0100

    move LastUpdate to a separate js file

---
+++ public/javascripts/lastupdate.js
@@ -0,0 +1,10 @@
+$(window).load(function() {
+
+    updateTime();
+
+    function updateTime() {
+        var d = new Date();
+        document.getElementById("LastUpdate").firstChild.nodeValue =
+        d.toTimeString().substring(0, 8) + " " + d.toLocaleDateString();
+    }
+});
